864ab76cc7f46e94d3815d22545ba951b838edcd
Shadrak Gurupnor <gurupnor.shadrak@gmail.com>
2021-01-03 22:24:12 +0530

    bootstrap added

--- react-frontend/src/Components/Modalbox.js
+++ react-frontend/src/Components/Modalbox.js
@@ -1,46 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Modal, Form } from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import { Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
-const Modalbox = ({title, options, textlabel, dropdownlabel, onchangeHandler}) => {
-    const [open, setOpen] = useState(false);
+const Modalbox = ({
+  title,
+  options,
+  textlabel,
+  dropdownlabel,
+  onClickHandler,
+}) => {
+  const [open, setOpen] = useState(false);
+//   const [data, setData] = useState([]);
 
-    return(
-        <Modal
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            open={open}
-            trigger={<Button floated='right'>{title}</Button>}
-        >
-            <Modal.Header>{title}</Modal.Header>
-            <Modal.Content>
-                <Form >
-                    <Form.Group widths='equal'>
-                        <Form.Field
-                            control={Select}
-                            label={dropdownlabel}
-                            options={options}
-                            placeholder={dropdownlabel}
-                            onChange={onchangeHandler}
-                        />
-                        <Form.Field
-                            control={Input}
-                            label={textlabel}
-                            placeholder={textlabel}
-                        />
-                    </Form.Group>
-                    <Button type='submit' onClick={() => newProduct()}>Submit</Button>
-                </Form>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button color='black' onClick={() => setOpen(false)}>
-                Cancel
-                </Button>
-                {/* <Button color='green' onClick={() => setOpen(false)}>
-                    Done
-                </Button> */}
-            </Modal.Actions>
-        </Modal>
-    );
-}
+  const toggle = () => setOpen(!open);
+  return (
+    // <div>hi</div>
+    <Modal isOpen={open} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+
+        <Form>
+            <FormGroup>
+                <Label>Select a Product</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                    {options.map(items => {
+                        <option>{items[1]}</option>
+                    })}
+                </Input>
+            </FormGroup>
+            <FormGroup>
+                <Label>Quantity</Label>
+                <Input type="number" name="quantity" id="quantity" placeholder="for example 10"/>
+            </FormGroup>
+            <Button type="submit" onClick={() => setOpen(false)}>Done</Button>
+        </Form>
+        </ModalBody> 
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+  );
+};
 
 export default Modalbox;
--- react-frontend/src/Pages/ProductMovement.js
+++ react-frontend/src/Pages/ProductMovement.js
@@ -2,17 +2,31 @@ import React, { useState, useEffect } from 'react';
 // import { Button, Modal, Form, ModalDescription } from 'semantic-ui-react';
 import axios from 'axios';
 import Dropdown from '../Components/Dropdown';
-import Modalbox from '../Components/Modalbox';
+import { Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+// import Modalbox from '../Components/Modalbox';
 
 const ProductMovemnent = () => {
 
     const [movements, setMovements] = useState([]);
     const [options, setOptions] = useState([]);
     const [location, setLocation] = useState('');
+    const [exportOptions, setExportOptions] = useState([]);
+    const [open, setOpen] = useState(false);
+
+    const toggle = () => setOpen(!open);
 
     useEffect(() => {
         fetchlocations();
     },[]);
+
+    useEffect(() => {
+        fetchMovements();
+    },[]);
+
+    useEffect(() => {
+        loca();
+    },[location]);
 
     const fetchlocations = async () => {
         const response = await fetch("/locations");
@@ -26,28 +40,31 @@ const ProductMovemnent = () => {
         setLocation(e.target.textContent);
     }
 
-    useEffect(() => {
-        loca();
-    },[location]);
+    
 
     const loca = () => {
         axios.get(`/productmovements?location=${location}`)
         .then(res => {
-            console.log(res);
             setMovements(res.data);
+            setExportOptions(res.data);
         })
         .catch(err => console.log(err));
     }
 
-    useEffect(() => {
-        fetchMovements();
-    },[]);
-
     const fetchMovements = async() => {
         const response = await fetch('/productmovement');
         const data = await response.json();
-        console.log(data);
         setMovements(data);
+    }
+
+    // const exportChangeHandler = (e,data) => {
+    //     // console.log(e);
+    //     e.preventDefault()
+    //     console.log(data)
+    // }
+
+    const formHandler = (e) => {
+        console.log(e);
     }
 
     return (
@@ -60,13 +77,48 @@ const ProductMovemnent = () => {
             options={options}
         />
         </div>
-        {/* <Button floated='right'>Import Item</Button>
+        <Button floated='right'>Import Item</Button>
         <Button floated='right'>Move Item</Button>
-        <Button floated='right'>Export Item</Button> */}
+        <Button floated='right' onClick={toggle}>Export Item</Button>
         
-        <Modalbox title={'Export Item'}/>
-        <Modalbox title={'Move Item'}/>
-        <Modalbox title={'Import Item'}/>
+        {/* <Modalbox title={'Export Item'}
+            options={exportOptions}
+            textlabel={'Enter Quantity'}
+            dropdownlabel={'Select Product'}
+            onClickHandler={exportChangeHandler}
+        /> */}
+        {/* <Modalbox title={'Move Item'}/> */}
+        {/* <Modalbox title={'Import Item'}
+            options={}
+            textlabel={}
+            dropdownlabel={}
+            onchangeHandler={}
+        /> */}
+
+    <Modal isOpen={open} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Export Item</ModalHeader>
+        <ModalBody>
+
+        <Form onSubmit={formHandler}>
+            <FormGroup>
+                <Label>Select a Product</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                    {options.map(items => {
+                        <option>{items[1]}</option>
+                    })}
+                </Input>
+            </FormGroup>
+            <FormGroup>
+                <Label>Quantity</Label>
+                <Input type="number" name="quantity" id="quantity" placeholder="for example 10"/>
+            </FormGroup>
+            <Button type="submit" onClick={() => setOpen(false)}>Done</Button>
+        </Form>
+        </ModalBody> 
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
         </div>
         
         <div className="table-box">
